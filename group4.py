82d43191967fd6147d86bf4fd24032cf8c4faa30
D Pedroja <daron.pedroja@waterboards.ca.gov>
2021-02-26 15:09:53 -0800

    flag 23 draft complete

--- group4.py
+++ group4.py
@@ -5,6 +5,7 @@ Created on Thu Feb 25 10:00:14 2021
 @author: dpedroja
 """
 
+# THIS SCRIPT CAN TAKE SEVERAL MINUTES OR MORE
 
 import pandas as pd
 import numpy as np
@@ -52,44 +53,29 @@ for i, use in enumerate(vals):
 flag_23_data = flat_file_season
 # replace NaN with 0
 flag_23_data = flag_23_data.fillna(0)
+# define diversion seasons
+season_start = ["DIRECT_SEASON_START_MONTH_1", "DIRECT_SEASON_START_MONTH_2", "DIRECT_SEASON_START_MONTH_3"]
+season_end = ["DIRECT_DIV_SEASON_END_MONTH_1", "DIRECT_DIV_SEASON_END_MONTH_2", "DIRECT_DIV_SEASON_END_MONTH_3"]
+# define fields
+div_fields = ["DIVERSION_MONTHS", "DIVERSION_MONTHS_2", "DIVERSION_MONTHS_3"]
+for field in div_fields:
+    flag_23_data[field] = [[]] *len(flag_23_data) 
 
 # generate list of lists
-flag_23_data["DIVERSION_MONTHS"] = [[]] * len(flag_23_data) 
-for i in range(len(flag_23_data)):
-    months = range(int(flag_23_data["DIRECT_SEASON_START_MONTH_1"][i]), 
-    int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_1"][i]+1))
-    flag_23_data["DIVERSION_MONTHS"][i] = [month for month in months] 
-
-for i in range(len(flag_23_data)):
-    if (flag_23_data["DIRECT_SEASON_START_MONTH_1"][i] > flag_23_data["DIRECT_DIV_SEASON_END_MONTH_1"][i]):
-        months = range(int(flag_23_data["DIRECT_SEASON_START_MONTH_1"][i]), 13) 
-        months2 = range(1, int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_1"][i]))
-        flag_23_data["DIVERSION_MONTHS"][i] = [month for month in months]
-        months2 = [month for month in months2]
-        for element in months2:
-            flag_23_data["DIVERSION_MONTHS"][i].append(element)
-
-flag_23_data["DIVERSION_MONTHS_2"] = [[]] * len(flag_23_data) 
-for i in range(len(flag_23_data)):
-    if (flag_23_data["DIRECT_SEASON_START_MONTH_2"][i] > flag_23_data["DIRECT_DIV_SEASON_END_MONTH_2"][i]):
-        months = range(int(flag_23_data["DIRECT_SEASON_START_MONTH_2"][i]), 13) 
-        months2 = range(1, int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_2"][i]))
-    # int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_1"][i]+1))
-        flag_23_data["DIVERSION_MONTHS_2"][i] = [month for month in months]
-        months2 = [month for month in months2]
-        for element in months2:
-            flag_23_data["DIVERSION_MONTHS_2"][i].append(element)
-
-flag_23_data["DIVERSION_MONTHS_3"] = [[]] * len(flag_23_data) 
-for i in range(len(flag_23_data)):
-    if (flag_23_data["DIRECT_SEASON_START_MONTH_3"][i] > flag_23_data["DIRECT_DIV_SEASON_END_MONTH_3"][i]):
-        months = range(int(flag_23_data["DIRECT_SEASON_START_MONTH_3"][i]), 13) 
-        months2 = range(1, int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_3"][i]))
-    # int(flag_23_data["DIRECT_DIV_SEASON_END_MONTH_1"][i]+1))
-        flag_23_data["DIVERSION_MONTHS_3"][i] = [month for month in months]
-        months2 = [month for month in months2]
-        for element in months2:
-            flag_23_data["DIVERSION_MONTHS_3"][i].append(element)
+# TAKES A FEW MINUTES
+for k, field in enumerate(season_start):
+    for i in range(len(flag_23_data)):
+        months = range(int(flag_23_data[season_start[k]][i]), 
+        int(flag_23_data[season_end[k]][i]+1))
+        flag_23_data[div_fields[k]][i] = [month for month in months] 
+    for i in range(len(flag_23_data)):
+        if (flag_23_data[season_start[k]][i] > flag_23_data[season_end[k]][i]):
+            months = range(int(flag_23_data[season_start[k]][i]), 13) 
+            months2 = range(1, int(flag_23_data[season_end[k]][i]))
+            flag_23_data[div_fields[k]][i] = [month for month in months]
+            months2 = [month for month in months2]
+            for element in months2:
+                flag_23_data[div_fields[k]][i].append(element)
 # create month field names
 months = ["DIRECT_DIV_JANUARY", "DIRECT_DIV_FEBRUARY", "DIRECT_DIV_MARCH", "DIRECT_DIV_APRIL", 
           "DIRECT_DIV_MAY", "DIRECT_DIV_JUNE", "DIRECT_DIV_JULY", "DIRECT_DIV_AUGUST", 
@@ -97,7 +83,7 @@ months = ["DIRECT_DIV_JANUARY", "DIRECT_DIV_FEBRUARY", "DIRECT_DIV_MARCH", "DIRE
 for month in months:
     flag_23_data[month] = "NA"
 # check each month against list
-# THIS LOOP TAKES AT LEAST 5 MINUTES !!!!!!!!
+# COULD TAKE A WHILE (5 MINUTES OR MORE)
 for j, month in enumerate(months):
     for i in range(len(flag_23_data)):
         flag_23_data[month][i] = np.where(
@@ -105,45 +91,37 @@ for j, month in enumerate(months):
             ((j+1) in flag_23_data["DIVERSION_MONTHS_2"][i])  |
             ((j+1) in flag_23_data["DIVERSION_MONTHS_3"][i]),
             "Y", "N")
+# temporary
+flag_23_data.to_csv("eWRIMS_data/flag_23_data.csv")
+flag_23_data = pd.read_csv("eWRIMS_data/flag_23_data.csv")
+div_season_data = pd.DataFrame(flag_23_data[["WR_WATER_RIGHT_ID", "APPLICATION_NUMBER", "USE_CODE",
+        "DIRECT_DIV_JANUARY", "DIRECT_DIV_FEBRUARY", "DIRECT_DIV_MARCH", "DIRECT_DIV_APRIL", 
+        "DIRECT_DIV_MAY", "DIRECT_DIV_JUNE", "DIRECT_DIV_JULY", "DIRECT_DIV_AUGUST",
+        "DIRECT_DIV_SEPTEMBER", "DIRECT_DIV_OCTOBER", "DIRECT_DIV_NOVEMBER", "DIRECT_DIV_DECEMBER"]])
+del(flag_23_data)
 
-
-
-
-
-# merge data, etc.
-
-
+# import rms monthly data
 rms_monthly = imp.import_rms_monthly(100000000000000)
-rms_annual = imp.import_rms_annual(10000000000000)
-
-rms_annual.head(1000)
-flag_23_data.head(1000)
-rms_monthly.head(1000)
-
-
-
-
-
-
-
-flag_6a_data["RIPARIAN"] = np.where( 
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,")                    |  
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,OTHER,")              | 
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,COURTADJ,")           | 
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PENDING,")            |
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,COURTADJ,OTHER,")     |
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PENDING,OTHER,")      |   
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,")                    |  
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,OTHER,")              | 
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,COURTADJ,")           | 
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,PENDING,")            |
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,COURTADJ,OTHER,")     |
-    (flag_6a_data["SUB_TYPE"] == "RIPERIAN,PRE1914,PENDING,OTHER,"),
-    "Y", "N")
+rms_monthly.reset_index(inplace = True)
+# merge data
+flag_23_merge = rms_monthly.merge(div_season_data, how = "inner", left_on = "APPL_ID", right_on = "APPLICATION_NUMBER")
+# this merge multiplies a lot of records because of multiple use types per application number. 
+# rms monthly reported diversion fields
+div_reported = ["JANUARY_DIV", "FEBRUARY_DIV", "MARCH_DIV", "APRIL_DIV", 
+              "MAY_DIV", "JUNE_DIV", "JULY_DIV", "AUGUST_DIV", 
+              "SEPTEMBER_DIV", "OCTOBER_DIV", "NOVEMBER_DIV", "DECEMBER_DIV"]
+# Field DIV_REPORTED_OUTSIDE_DIV_SEASON criteria
+for n, month in enumerate(div_reported):
+    flag_23_merge["DIV_REPORTED_OUTSIDE_DIV_SEASON"] = np.where( 
+        (flag_23_merge[div_reported[n]] > 0)                          &  
+        (flag_23_merge[months[n]] == "N"),
+        "Y", "N")
+# remove use code so the table can be condensed
+flag_23_merge.drop(["USE_CODE"], axis = 1, inplace = True)
+# number of records is greater than RMS because some applications have different diversion months for differenct use codes (apparentely)
+flag_23_merge.drop_duplicates(inplace = True)
 
 
-
-# df = pd.DataFrame(
 #     {'trial_num': [1, 2, 3, 1, 2, 3],
 #      'subject': [1, 1, 1, 2, 2, 2],
 #      'samples': [list(np.random.randn(3).round(2)) for i in range(6)]
